28201e3bb4b3924576e525cc899261d8cc3ccdad
Azeez ADEBAYO <adebayoazeez37@yahoo.com>
2024-10-10 17:57:53 +0100

    changed the frontendUrl to the server url

--- config/variables.js
+++ config/variables.js
@@ -1,7 +1,13 @@
+const remoteFrontendUrl = "https://utility-service-frontend.vercel.app/";
+const localFrontendUrl = "http://127.0.0.1:3000";
 const endpoints = {
-  verifyOtp: "http://localhost:3000/verify-email-status",
-  passwordRecovery: "http://localhost:3000/password-recovery",
+  verifyOtp: `${remoteFrontendUrl}verify-email-status`,
+  passwordRecovery: `${remoteFrontendUrl}password-recovery`,
 };
+// const endpoints = {
+//   verifyOtp: `{localFrontendUrl}verify-email-status`,
+//   passwordRecovery: `{localFrontendUrl}password-recovery`,
+// };
 module.exports = {
   ...endpoints,
 };
